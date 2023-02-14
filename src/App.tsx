abefa96ae588647901e31c2508e6c6eb94e69999
Sugandha <raosugandha12@gmail.com>
2023-02-14 19:59:03 +0000

    displayed first person on page

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,18 @@ import './App.css';
 
 const App: React.FC = () => {
   const [swCharacter, setSwCharacter] = useState("Name");
+  const [error, setError] = useState("try again" );
 
   const getCharacters = async () => {
     const apiResponse = await fetch(`https://swapi.dev/api/people/1`);
-    const json = await apiResponse.json();
-    setSwCharacter(json.name);
+    if(apiResponse.ok){
+      const json = await apiResponse.json()
+      setSwCharacter(json.name)}
+      else if (apiResponse.status === 418) {
+        setError("418 I'm a tea pot 🫖");
+      
+    }
+   
   };
 
   useEffect(() => {
